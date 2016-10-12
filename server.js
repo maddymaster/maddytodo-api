60ca74494469027e83e8b10b6a5b000ebda9099b
maddymaster <maddymaster@gmail.com>
2016-10-13 00:06:56 +0530

    Added POST for Todos

--- server.js
+++ server.js
@@ -1,21 +1,12 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
-var PORT = process.env.PORT || 3029;
-var todos = [{
+var PORT = process.env.PORT || 3002;
+var todos = [];
+var todoNextId = 1;
 
-	id: 1,
-	description: 'Meet Matt for Lunch',
-	completed: false
-}, {
-	id: 2,
-	description: 'Go to Store',
-	completed: false
-}, {
-	id: 3,
-	description: 'Go some Grocery',
-	completed: false
-
-}];
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
 	res.send('Todo API Root');
@@ -44,6 +35,20 @@ app.get('/todos/:id', function (req, res){
 	}
 
 	
+});
+
+//POST /todos
+app.post('/todos', function (req, res) {
+	var body = req.body;
+
+	// add id field
+	body.id = todoNextId++;
+
+
+	// push body into array
+	todos.push(body);
+
+	res.json(body);
 });
 
 app.listen(PORT, function() {
